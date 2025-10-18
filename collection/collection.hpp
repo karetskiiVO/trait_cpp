2586404dc4b141e6c961131a502cc3b8de37c4ce
Karetskii Vlad <kar.vladi@yandex.ru>
2025-10-19 01:31:15 +0300

    initial

---
+++ collection/collection.hpp
@@ -0,0 +1,24 @@
+#include <iter.hpp>
+
+namespace coll {
+    #include <vector>
+    #include <set>
+    #include <functional>
+    #include <memory>
+    
+    template <typename CollectionT>
+    class Collection : public CollectionT {
+    public:
+        using CollectionT::CollectionT;
+
+        iter::Iter<iter::CollectionIter<CollectionT>> iter() {
+            return iter::Iter<iter::CollectionIter<CollectionT>>(*this);
+        }
+    };
+
+    template <class _Ty, class _Alloc = ::std::allocator<_Ty>>
+    using vector = Collection<std::vector<_Ty, _Alloc>>;
+
+    template <class _Kty, class _Pr = ::std::less<_Kty>, class _Alloc = ::std::allocator<_Kty>>
+    using set = std::set<_Kty, _pr, _Alloc>;
+};
